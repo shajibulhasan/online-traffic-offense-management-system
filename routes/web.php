dbcc705cee0656ef1337eec1c2c544421ccf59aa
shajibulhasan <shajibulhasan.puc@gmail.com>
2025-07-03 22:51:39 +0600

    District Lead Middleware added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\OfficerController;
 use App\Http\Middleware\ValidateUser;
 use App\Http\Middleware\ValidateOfficer;
 use App\Http\Middleware\DivisionLead;
+use App\Http\Middleware\DistrictLead;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,21 +20,23 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/Admin/verifyOfficerAccount', [AdminController::class, 'verifyOfficerAccount'])->name('Admin.verifyOfficerAccount')->middleware(DivisionLead::class);
 Route::get('/Admin/assignDistrictList', [AdminController::class, 'AssignDistrictList'])->name('Admin.assignDistrictList')->middleware(DivisionLead::class);
 Route::get('/Admin/assignDistrict', [AdminController::class, 'assignDistrict'])->name('Admin.assignDistrict')->middleware(DivisionLead::class);
+
+Route::get('/Admin/assignThana', [AdminController::class, 'assign_thana'])->name('Admin.assignThana')->middleware(DistrictLead::class);
+Route::get('/Admin/show-assign-thana', [AdminController::class, 'showing_assign_thana'])->name('Admin.show-assign-thana')->middleware(DistrictLead::class);
+Route::get('/Admin/addThana', [AdminController::class, 'addThana'])->name('Admin.addThana')->middleware(DistrictLead::class);
+Route::get('/Admin/thanaList', [AdminController::class, 'thanaList'])->name('Admin.thanaList')->middleware(DistrictLead::class);
+
 Route::get('/Admin/index', [AdminController::class, 'index'])->name('Admin.index')->middleware(ValidateOfficer::class);
-Route::get('/Admin/addThana', [AdminController::class, 'addThana'])->name('Admin.addThana')->middleware(ValidateOfficer::class);
 Route::get('/Admin/divission', [AdminController::class, 'divission'])->name('Admin.divission')->middleware(ValidateOfficer::class);
 Route::get('/Admin/addArea', [AdminController::class, 'addArea'])->name('Admin.addArea')->middleware(ValidateOfficer::class);
 Route::get('/Admin/areaList', [AdminController::class, 'areaList'])->name('Admin.areaList')->middleware(ValidateOfficer::class);
 Route::get('/Admin/assignOfficer', [AdminController::class, 'assignOfficer'])->name('Admin.assignOfficer')->middleware(ValidateOfficer::class);
 Route::get('/Admin/assignOfficerList', [AdminController::class, 'assignOfficerList'])->name('Admin.assignOfficerList')->middleware(ValidateOfficer::class);
-Route::get('/Admin/thanaList', [AdminController::class, 'thanaList'])->name('Admin.thanaList')->middleware(ValidateOfficer::class);
-Route::get('/Admin/assignThana', [AdminController::class, 'assign_thana'])->name('Admin.assignThana')->middleware(ValidateOfficer::class);
-Route::get('/Admin/show-assign-thana', [AdminController::class, 'showing_assign_thana'])->name('Admin.show-assign-thana')->middleware(ValidateOfficer::class);
 Route::get('/Admin/areaList', [AdminController::class, 'areaList'])->name('Admin.areaList')->middleware(ValidateOfficer::class);
 Route::get('/Admin/updateThana/{id}', [AdminController::class, 'UpdateThana'])->name('Admin.updateThana')->middleware(ValidateOfficer::class);
 Route::get('/Admin/updateArea/{id}', [AdminController::class, 'UpdateArea'])->name('Admin.updateArea')->middleware(ValidateOfficer::class);
 Route::get('/Admin/updateAssignDistrict/{id}', [AdminController::class, 'updateAssignDistrict'])->name('Admin.updateAssignDistrict')->middleware(ValidateOfficer::class);
-Route::get('/Admin/assignThana',[AdminController::class, 'assignThana'])->name('Admin.assignThana')->middleware(ValidateOfficer::class);
+// Route::get('/Admin/assignThana',[AdminController::class, 'assignThana'])->name('Admin.assignThana')->middleware(ValidateOfficer::class);
 Route::get('/Admin/assignOfficerList', [AdminController::class, 'areaOficerList'])->name('Admin.assignOfficerList')->middleware(ValidateOfficer::class);
 Route::get('/Admin/updateAssignOfficer', [AdminController::class, 'updateAssignOfficer'])->name('Admin.updateAssignOfficer')->middleware(ValidateOfficer::class);
 // Show edit page
