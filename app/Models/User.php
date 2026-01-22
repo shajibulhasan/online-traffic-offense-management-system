0033591e4647927cf90b3cc3292fe19536aa85eb
Shajibul Hasan Soaib <shajibulhasan.puc@gmail.com>
2026-01-22 23:19:09 +0600

    integrate app with login and register api

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    // use HasFactory, Notifiable;
 
+        use HasApiTokens, HasFactory, Notifiable;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -27,6 +30,9 @@ class User extends Authenticatable
         'password',
         'nid',
     ];
+
+
+
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
